4a9787be7373f2d4ecfac1586e4acae8e87f2a9d
grigoremihaela <grigoremihaela@ymail.com>
2018-04-05 18:21:10 +0300

    back

--- index.js
+++ index.js
@@ -16,12 +16,14 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
+  /*
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
-console.log(path.join(__dirname, 'index.html'));
+  */
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
